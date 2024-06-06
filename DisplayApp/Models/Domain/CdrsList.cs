67b1dbcc414c55e319caf12fb3174e3fd678120f
MrIncognito022 <101396617+MrIncognito022@users.noreply.github.com>
2024-06-07 01:43:34 +0500

    sdvsdv

---
+++ DisplayApp/Models/Domain/CdrsList.cs
@@ -0,0 +1,67 @@
+﻿namespace DisplayApp.Models.Domain
+{
+    public class CdrsList
+    {
+        public string Scustomer { get; set; }
+        public string Recording { get; set; }
+        public string OutgroupName { get; set; }
+        public string BillType { get; set; }
+        public string Overmax { get; set; }
+        public string CalleridExternal { get; set; }
+        public decimal CostIncludingTax { get; set; }
+        public string Dtype { get; set; }
+        public string Totaltime { get; set; }
+        public string SnumberName { get; set; }
+        public string Callername { get; set; }
+        public string Dnumber { get; set; }
+        public int Spresent { get; set; }
+        public string Media { get; set; }
+        public string CnumberDisplay { get; set; }
+        public string Plan { get; set; }
+        public string Server { get; set; }
+        public string DnumberDisplay { get; set; }
+        public decimal Cost { get; set; }
+        public string Ctype { get; set; }
+        public int Messages { get; set; }
+        public string IngroupTime { get; set; }
+        public string OwnerCost { get; set; }
+        public string Uniqueid { get; set; }
+        public string Archived { get; set; }
+        public string Taxed { get; set; }
+        public string End { get; set; }
+        public string Outgroup { get; set; }
+        public string Ingroup { get; set; }
+        public decimal Balance { get; set; }
+        public string Invoice { get; set; }
+        public string Dcustomer { get; set; }
+        public string Cnumber { get; set; }
+        public string CustomerName { get; set; }
+        public long Start { get; set; }
+        public string Snumber { get; set; }
+        public string SnumberDisplay { get; set; }
+        public string DnumberName { get; set; }
+        public int Time { get; set; }
+        public string Status { get; set; }
+        public string Stype { get; set; }
+        public string Channel { get; set; }
+        public string CnumberName { get; set; }
+        public string CalleridInternal { get; set; }
+        public string Currency { get; set; }
+        public string OwnerCurrency { get; set; }
+        public string Sname { get; set; }
+        public string CostExcludingTax { get; set; }
+        public int MessagesUsed { get; set; }
+        public string Callid { get; set; }
+        public int SecondsUsed { get; set; }
+        public string BridgedCallid { get; set; }
+        public string OwnerCostIncludingTax { get; set; }
+        public string BillRef { get; set; }
+        public string Note { get; set; }
+        public string OutgroupTime { get; set; }
+        public string Customer { get; set; }
+        public string Talktime { get; set; }
+        public int Peer { get; set; }
+        public string OwnerCostExcludingTax { get; set; }
+
+    }
+}
