2ff524991fac10a9fa414589c11f1e63a5f28055
Ahmed Qreshi <mr.incognito022@gmail.com>
2024-06-07 13:50:32 +0500

    Navbar Done

--- DisplayApp/Controllers/CallsController.cs
+++ DisplayApp/Controllers/CallsController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -6,13 +8,6 @@ namespace DisplayApp.Controllers
 {
     public class CallsController : Controller
     {
-        private readonly IHttpClientFactory _clientFactory;
-
-        public CallsController(IHttpClientFactory clientFactory)
-        {
-            _clientFactory = clientFactory;
-        }
-
         [HttpGet]
         public IActionResult CdrsList()
         {
@@ -41,22 +36,44 @@ namespace DisplayApp.Controllers
             long startTimestamp = new DateTimeOffset(startDateTime).ToUnixTimeSeconds();
             long endTimestamp = new DateTimeOffset(endDateTime).ToUnixTimeSeconds();
 
-            var request = new HttpRequestMessage(HttpMethod.Get,
-                $"https://www.015pbx.net/local/api/json/cdrs/list/?auth_username={username};auth_password={password};start={startTimestamp};end={endTimestamp}");
-
-            var client = _clientFactory.CreateClient();
-            var response = await client.SendAsync(request);
-
-            if (response.IsSuccessStatusCode)
+            var handler = new HttpClientHandler
             {
-                var content = await response.Content.ReadAsStringAsync();
-                return Json(new { success = true, data = content });
-            }
-            else
+                UseProxy = true,
+                Proxy = new WebProxy("http://10.50.151.251:2356", true)
+                {
+                    Credentials = new NetworkCredential("13068", "13068")
+                },
+                UseDefaultCredentials = false
+            };
+
+            handler.Proxy.Credentials = new NetworkCredential("13068", "13068");
+
+            using (var client = new HttpClient(handler))
             {
-                return Json(new { success = false, message = "Error fetching data" });
+                var request = new HttpRequestMessage(HttpMethod.Get,
+                    $"https://www.015pbx.net/local/api/json/cdrs/list/?auth_username={username}&auth_password={password}&start={startTimestamp}&end={endTimestamp}");
+
+                try
+                {
+                    var response = await client.SendAsync(request);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        return Json(new { success = true, data = content });
+                    }
+                    else
+                    {
+                        return Json(new { success = false, message = "Error fetching data" });
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    // Log or handle the exception as needed
+                    Console.WriteLine($"Proxy connection error: {ex.Message}");
+                    return StatusCode(StatusCodes.Status500InternalServerError, new { message = "Error connecting to API through proxy" });
+                }
             }
         }
-
     }
 }
