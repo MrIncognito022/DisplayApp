50227590702097b05b7fb69576975f3a5d8b2d62
MrIncognito022 <101396617+MrIncognito022@users.noreply.github.com>
2024-06-18 14:08:10 +0500

    dvsdmvlsdvl

---
+++ DisplayApp/Controllers/TranscriptionController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace DisplayApp.Controllers
+{
+    public class TranscriptionController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public TranscriptionController()
+        {
+            _httpClient = new HttpClient();
+        }
+        [HttpPost]
+        public async Task<IActionResult> TranscribeAudio([FromBody] string encodedAudioData)
+        {
+            return Ok(encodedAudioData);
+        }
+    }
+}
